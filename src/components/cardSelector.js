dbf5b3fc713de78e41c6db62bf299fc4ec3fd64a
Bryan Gomes <bryanpgomes@gmail.com>
2025-07-03 18:37:23 -0400

    Fix import

--- src/components/cardSelector.js
+++ src/components/cardSelector.js
@@ -2,7 +2,7 @@ function DeckSelector(packData) {
     // Pack data needs to be processed here
     // I need to create a deck of at least 30 cards
     // I need to loop through each pack, and organize the cards by type
-    
+
     return (
         <div>
             <h1>Deck Selector</h1>
@@ -10,4 +10,4 @@ function DeckSelector(packData) {
     );
 }
 
-export default CardSelector;
+export default DeckSelector;
